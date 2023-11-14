8c731b518d90c323723d01398e348c7673afb04d
rStar <starlove124760@gmail.com>
2023-11-14 13:38:27 +0800

    add rigidbody collider for player  and floor

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,18 +11,13 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        Debug.Log("Start");
+        // Debug.Log("Start");
     }
 
     // Update is called once per frame
     private void Update()
     {
-        Debug.Log("Update");
-        var direction = new Vector3(1 , 0 , 0);
-        var moveSpeed = 5;
-        var fps       = Time.deltaTime;
-        var movement  = direction * fps * moveSpeed;
-        transform.position += movement;
+        // Debug.Log("Update");
     }
 
 #endregion
