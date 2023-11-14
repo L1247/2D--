48841f3c4a6cb138958b12cf510ab62c3cb56e0c
rStar <starlove124760@gmail.com>
2023-11-14 14:47:03 +0800

    Make player character Jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,8 @@ public class PlayerMovement : MonoBehaviour
 {
 #region Public Variables
 
+    public float jumpY;
+
     public int moveSpeed;
 
     public Rigidbody2D rigidbody2D;
@@ -21,9 +23,19 @@ public class PlayerMovement : MonoBehaviour
     {
         var horizontal = Input.GetAxisRaw("Horizontal");
         var movementX  = horizontal * moveSpeed;
-        print(movementX);
+        // print(movementX);
         var newVelocity = new Vector2(movementX , rigidbody2D.velocity.y);
         rigidbody2D.velocity = newVelocity;
+
+        var jumpKeyDown = Input.GetKeyDown(KeyCode.Space);
+        if (jumpKeyDown)
+        {
+            print("空白鍵按下");
+            var y = 100 * jumpY;
+
+            var jumpForce = new Vector2(0 , y);
+            rigidbody2D.AddForce(jumpForce);
+        }
     }
 
 #endregion
