47dfebf0f1f6dd4a6fdfc7b407ab18f3efe2d611
rStar <starlove124760@gmail.com>
2023-11-28 14:46:25 +0800

    missing commit

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,30 @@
+#region
+
+using UnityEngine;
+
+#endregion
+
+public class PlayerMovement : MonoBehaviour
+{
+#region Public Variables
+
+    public int moveSpeed;
+
+    public Rigidbody2D rigidbody2D;
+
+#endregion
+
+#region Unity events
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        var movementX  = horizontal * moveSpeed;
+        // print(movementX);
+        var newVelocity = new Vector2(movementX , rigidbody2D.velocity.y);
+        rigidbody2D.velocity = newVelocity;
+    }
+
+#endregion
+}
---
+++ Assets/Scripts/UI/Dialog.cs
@@ -0,0 +1,28 @@
+#region
+
+using TMPro;
+using UnityEngine;
+
+#endregion
+
+public class Dialog : MonoBehaviour
+{
+#region Public Variables
+
+    public TMP_Text dialogText;
+
+#endregion
+
+#region Unity events
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        dialogText.text = "中文測試";
+    }
+
+    // Update is called once per frame
+    private void Update() { }
+
+#endregion
+}
