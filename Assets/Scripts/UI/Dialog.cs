a4e3c7248d5a96f79bfe3ab219470bb282a26ebb
rStar <starlove124760@gmail.com>
2023-12-05 13:53:30 +0800

    change line index

--- Assets/Scripts/UI/Dialog.cs
+++ Assets/Scripts/UI/Dialog.cs
@@ -10,6 +10,11 @@ public class Dialog : MonoBehaviour
 {
 #region Public Variables
 
+    /// <summary>
+    ///     句子的索引，紀錄目前在陣列第幾個元素
+    /// </summary>
+    public int sentenceIndex;
+
     public List<string> sentences;
 
     public TMP_Text dialogText;
@@ -21,13 +26,20 @@ public class Dialog : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        dialogText.text = sentences[0];
+        // 設定文字為陣列第一句
+        dialogText.text =  sentences[sentenceIndex];
+        sentenceIndex   += 1; // index + 1
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0)) dialogText.text = "新句子";
+        // 左鍵按下
+        if (Input.GetMouseButtonDown(0))
+        {
+            dialogText.text =  sentences[sentenceIndex];
+            sentenceIndex   += 1; // index + 1
+        }
     }
 
 #endregion
